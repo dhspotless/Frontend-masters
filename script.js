c17bbf3818d4a24be9f7d7021946a0e7b313b06f
dhspotless <dhspotless@gmail.com>
2022-12-17 10:40:32 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// const monthlyRent = 500;
+// const yearlyRent = monthlyRent * 12
+// // document.addEventListener('click', function() {
+// //     dispalyTotal.style.color = 'purple'
+// //     dispalyTotal.textContent = (`Total: ${yearlyRent}`)
+// // })
+// document.querySelector('#totalBtn').onclick = function() {
+//     document.querySelector('h1').textContent = (`Total: ${yearlyRent}`)
+// }
+
+// document.querySelector('#restBtn').onclick = function() {
+//     document.querySelector('h1').textContent = ('')
+// }
+
+// let firstName = 'wisdom '
+// let lastName = 'samuel'
+// let h3El = document.querySelector('h3')
+
+// let fullName = firstName + lastName
+// h3El.textContent = (` Hello ${fullName}, How are you doing today bro?`)
+
+// let isSkyBlue = true;
+// let h3El = document.querySelector('h3')
+
+// if(isSkyBlue){
+//     h3El.textContent = ( `The sky is blue!`)
+// }else{
+//     h3El.textContent = ( `The sky is not blue?`)
+// // }
+// document.querySelector('body').style.backgroundColor = 'lightblue'
+// let h3El = document.querySelector('h3')
+
+// WHILE LOOP
+
+// let friendsAtYourParty = 0
+
+// while (friendsAtYourParty < 10){
+
+//     friendsAtYourParty ++
+//     console.log(friendsAtYourParty)
+// }
+// h3El.textContent = friendsAtYourParty
+// // h3El.textContent = friendsAtYourParty
+
+// }
+
+// FOR LOOP
+
+// document.querySelector("body").style.backgroundColor = "lightblue";
+
+// let h3El = document.querySelector("h3");
+
+// let friendsAtYourParty = 0;
+
+// for (let i = 0; i < 10; i++) {
+//   friendsAtYourParty++;
+//   console.log(i);
+// }
+// h3El.textContent = friendsAtYourParty;
+
+/*
+  Write some code that declares two variables, character and timesToRepeact.
+  Using a loop, repeat that character that many times and then console.log it.
+  Example, if I had character = 'f' and timesToRepeat = 5, it'd console.log
+  'fffff'
+*/
+
+// let character = "w";
+// let timesToRepeact = 5;
+// let reply = "";
+
+// for (let i = 0; i < timesToRepeact; i++) {
+//   reply += character;
+// }
+// console.log(reply);
+
+// FUCNTIONS STARTS HERE
+
+// let add = function () {
+//   console.log("i am blessed");
+// };
+// console.log(add());
+
+// function addMe(name, number) {
+//   return `hello ${name} can i have your ${number}`;
+// }
+
+// console.log(addMe("john", "mobile"));
+
+// FUNCTIONS ENDS HERE
+
+//
+// javascript arrow function?
+
+// arrow function
